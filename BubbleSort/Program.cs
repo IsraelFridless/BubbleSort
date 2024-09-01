2a3e88e1786019afdcfa5120a2b685913afe7ef2
Israel <if0583275151@gmail.com>
2024-09-01 10:40:26 +0300

    Selection sort added

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(string.Join(", ", BubbleSort([5, 8, 2, 4, 1, 0, 10])));
+            Console.WriteLine(string.Join(", ", SelectionSort([5, 8, 2, 4, 1, 0, 10])));
+        }
+
+        public static int[] SelectionSort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    if (nums[i] < nums[j])
+                    {
+                        int temp = nums[i]; nums[i] = nums[j]; nums[j] = temp;
+                    }
+                }
+            }
+            return nums;
         }
 
         public static int[] BubbleSort(int[] nums)
